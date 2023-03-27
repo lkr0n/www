c450f492e9e174ca088359b8129cf9f1e56c1f70
Louis Kronberg <louiskronberg@gmail.com>
2023-03-27 14:40:30 +0200

    change website title, tweak css and fix image

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'orbital';    
+export const SITE_TITLE = 'the orbital';
 export const SITE_DESCRIPTION = 'Personal Website and blog of Louis Kronberg';
